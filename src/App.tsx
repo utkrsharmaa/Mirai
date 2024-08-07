17bcd48b009859d6febebf431e24dbc8044de37d
utkrsharmaa <utkrsharmaa@gmail.com>
2024-08-07 13:17:08 +0530

    fixed service layer fetching with advanced tag options

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App(): JSX.Element {
-  return(
-    <>
-    </>
-  )
-}
+import React from 'react';
+import AdvancedSearch from './components/AdvancedSearch';
+
+const App: React.FC = () => {
+  return (
+    <AdvancedSearch
+      title="Jujutsu Kaisen" 
+      includedTagNames={['Action']} 
+      excludedTagNames={['Romance']} 
+    />
+  );
+};
 
 export default App;
--- src/services/api/MangaDex/searchManga.ts
+++ src/services/api/MangaDex/searchManga.ts
@@ -1,32 +1,40 @@
-import axios from "axios";
+import axios from 'axios';
+  // search options for manga lookup
+  interface SearchOptions {
+    title: string;
+    includedTagNames?: string[],
+    excludedTagNames?: string[],
+  
+  }
+  // Define the type for the tag
+  type Tag = {
+    id: string;
+    attributes: {
+      name: {
+        en: string;
+      };
+    };
+  };
 
-// search options for manga lookup
-interface SearchOptions {
-  title: "Jujutsu Kaisen";
-  includedTagNames?: string[],
-  excludedTagNames?: string[],
-
-}
-
-export default async function searchManga ({title, includedTagNames, excludedTagNames}: SearchOptions): Promise<any> {
-  const baseUrl = 'https://api.mangadex.org';
-  //transform Tag arrays into arrays of UUIDs using the /manga/tags resource.
-  const tags = await axios(`${baseUrl}/manga/tag`);
-  // fetching & cross-checking selected included/excluded tags for advanced search
-  const includedTagIDs:any = tags.data.data
-    .filter((tag: { attributes: { name: { en: string; }; }; }) => includedTagNames?.includes(tag.attributes.name.en))
-    .map((tag: {attributes: {names: {en: string;};};}) => includedTagNames?.includes(tag.attributes.names.en))
-  const excludedTagIDs:any = tags.data.data
-    .filter((tag: { attributes: { name: { en: string; }; }; }) => excludedTagNames?.includes(tag.attributes.name.en))
-    .map((tag: {attributes: {names: {en: string;};};}) => excludedTagNames?.includes(tag.attributes.names.en))
-  // declaring search Parameters for fetch
-  const params: any = {title, includedTagIDs, excludedTagIDs}
-  // sending the request
-  const resp = await axios ({
-    method: 'GET',
-    url: `${baseUrl}/manga`,
-    params,
-  });
-
-  console.log((resp.data.data.manga));
-}
+  export default async function SearchManga ({title, includedTagNames, excludedTagNames}: SearchOptions): Promise<any> {
+    const baseUrl = 'https://api.mangadex.org';
+    //transform Tag arrays into arrays of UUIDs using the /manga/tags resource.
+    const tags = await axios(`${baseUrl}/manga/tag`);
+    // fetching & cross-checking selected included/excluded tags for advanced search
+    const includedTagIDs:any = tags.data.data
+      .filter((tag: Tag) => includedTagNames?.includes(tag.attributes.name.en))
+      .map((tag: Tag)=> tag.id)
+    const excludedTagIDs:any = tags.data.data
+      .filter((tag: Tag) => excludedTagNames?.includes(tag.attributes.name.en))
+      .map((tag: Tag) => tag.id)
+    // declaring search Parameters for fetch
+    const params: any = {title, includedTags: includedTagIDs, excludedTags: excludedTagIDs}
+    // sending the request to get manga data object
+    const resp = await axios ({
+      method: 'GET',
+      url: `${baseUrl}/manga`,
+      params,
+    });
+    console.log(resp.data.data);
+    return (resp.data.data);
+  }
