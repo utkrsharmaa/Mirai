50f42519ac5603fd9e0264479ce1341fb6be2b54
Utkarsh Sharma <utkrsharmaa@gmail.com>
2025-04-11 10:07:27 +0530

    initialized Eframe Egui for UI

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,34 @@
-// Initial state
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Manga Reader",
+        native_options,
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
+    )
+}
+
+struct MyApp {
+    name: String,
+}
+
+impl MyApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            name: "Hello".to_string(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Mirai");
+            ui.text_edit_singleline(&mut self.name);
+            if ui.button("Click me").clicked() {
+                self.name = "Clicked!".to_string();
+            };
+        });
+    }
 }
